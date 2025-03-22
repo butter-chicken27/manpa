6355b14296b905be6fa92effcb9e79d88b22eedc
Yash <ypt7895@gmail.com>
2025-03-22 10:21:12 +0530

    base auth setup

--- app/main.py
+++ app/main.py
@@ -1,11 +1,63 @@
-from fastapi import FastAPI
+import uvicorn
+from contextlib import asynccontextmanager
+
+from fastapi import Depends, FastAPI
 from models import MsgPayload
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
+from db import User, create_db_and_tables
+from schemas import UserCreate, UserRead, UserUpdate
+from users import (
+    SECRET,
+    auth_backend,
+    current_active_user,
+    fastapi_users,
+    google_oauth_client,
+)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Not needed if you setup a migration system like Alembic
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 messages_list: dict[int, MsgPayload] = {}
 
 app.mount("/app",StaticFiles(directory="static",html = True),name="static")
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+app.include_router(
+    fastapi_users.get_oauth_router(google_oauth_client, auth_backend, SECRET),
+    prefix="/auth/google",
+    tags=["auth"],
+)
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
 
 @app.get("/")
 def root() -> dict[str, str]:
@@ -32,3 +84,6 @@ def add_msg(msg_name: str) -> dict[str, MsgPayload]:
 @app.get("/messages")
 def message_items() -> dict[str, dict[int, MsgPayload]]:
     return {"messages:": messages_list}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
